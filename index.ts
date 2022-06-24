e54e7c9bbcc93ff366524b4efe7722c8510bf269
assad93 <samir_caverna@hotmail.com>
2022-06-24 09:09:17 -0300

    config server and create a first route

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("test");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (request: Request, response: Response) =>
+  response.json({ message: "Welcome to api-dio" })
+);
+
+app.listen(5000, () =>
+  console.log("Server is running in port 5000: http://localhost:5000/")
+);
